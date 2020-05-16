f46c4c2ef68352ad7bf98c539c7b70622f497bea
Darren CHAN <xuanzz@msn.com>
2020-05-16 20:04:09 +0800

    Init.

---
+++ ONE-Shield.cpp
@@ -0,0 +1,61 @@
+#include "Arduino.h"
+#include"ONE-Shield.h"
+
+LED::LED(String LED_Name)
+{
+	if (LED_Name == "left" || LED_Name == "green" || LED_Name == "g" || LED_Name == "l")
+	{
+		  _pin = left_LED_Pin;
+	}
+		if (LED_Name == "right" || LED_Name == "red" || LED_Name == "r")
+	{
+		  _pin = right_LED_Pin;
+	}
+  pinMode(_pin, OUTPUT);
+}
+
+void LED::on()
+{
+  digitalWrite(_pin,HIGH);
+}
+
+void LED::off()
+{
+  digitalWrite(_pin,LOW);
+}
+
+
+Motor::Motor(int Motor_Name)
+{
+	if (Motor_Name == 1)
+	{
+		PWM = PWMA;
+		IN1 = AIN1;
+		IN2 = AIN2;
+	}
+		if (Motor_Name == 2)
+	{
+		PWM = PWMB;
+		IN1 = BIN1;
+		IN2 = BIN2;
+	}
+  pinMode(PWM, OUTPUT);
+    pinMode(IN1, OUTPUT);
+	  pinMode(IN2, OUTPUT);
+}
+
+void Motor::on(int direction)
+{
+Motor::turn(defaultSpeed);
+}
+void Motor::off()
+{
+	analogWrite(PWM,0);
+}
+void Motor::turn(int motorValue)
+{
+	if (motorValue<0){digitalWrite(IN1,HIGH-reversed);digitalWrite(IN2,reversed-LOW);motorValue=-motorValue;}
+	else if (motorValue>0){digitalWrite(IN1,reversed-LOW);digitalWrite(IN2,HIGH-reversed);}
+	analogWrite(PWM,constrain(motorValue,0,255));
+	digitalWrite(EN,HIGH);
+}
---
+++ ONE-Shield.h
@@ -0,0 +1,46 @@
+/*
+ONE-Shield library V0.2
+All rights reserved by One Degree Production Service Limited
+onedpro.com
+Last Update: 02/12/2018
+Author: Darren Chan
+*/
+
+#ifndef ONE_Shield_h
+#define ONE_Shield_h
+#include "Arduino.h"
+//Pin definitions
+#define left_LED_Pin 2
+#define right_LED_Pin 3
+#define		PWMA  6
+#define		AIN1  8
+#define		AIN2  7
+#define		PWMB  9
+#define		BIN1  16
+#define		BIN2  10
+#define 	EN	15
+
+class LED
+{
+public:
+LED(String LED_Name);
+void on();
+void off();
+private:
+int _pin;
+};
+
+class Motor
+{
+public:
+Motor(int Motor_Name);
+void on(int direction = 1);
+void off();
+void turn(int motorValue);
+bool reversed = 0;
+int defaultSpeed = 255;
+private:
+int PWM,IN1,IN2;
+};
+
+#endif
